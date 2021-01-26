d7c76f5ad91a6a98229f88e98008e16542bd914d
hyauisgdasda <77517989+hyauisgdasda@users.noreply.github.com>
2021-01-25 21:21:21 -0300

    Add files via upload

---
+++ help.js
@@ -0,0 +1,24 @@
+
+const help = (prefix) => { 
+	return `
+    •••••••꧁TOWER BOT ৣ꧂••••••
+    ⟞⟦───────────────
+    ║╭──꧁✞*MAKER*✞꧂──🔆
+    ║╠➣ *${prefix}ocr*
+    ║╠➣ *${prefix}figurinha*
+    ║╠➣ *${prefix}tts*
+    ║╠➣ *${prefix}toimg*
+    ║╠➣ *${prefix}phlogo*
+    ║╠➣ *${prefix}quotemaker*
+    ║╠➣ *${prefix}ninjalogo*
+    ║╠➣ *${prefix}glitch*
+    ║╠➣ *${prefix}text3d*
+×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=              
+
+`
+}
+exports.help = help
+
+
+
+  
---
+++ src/help.js
@@ -0,0 +1,31 @@
+
+const help = (prefix) => { 
+	return `  
+ ㅤㅤㅤㅤㅤㅤㅤㅤ 🌍
+    •••••••꧁ঔৣTOWER BOTঔৣ꧂••••••
+
+    ╭──꧁✞ COMANDOS ✞꧂──🔆
+    ║
+    ⟞⟦────────────────🔰
+    ║ ꧁ঔৣ✞TOWER BOT✞ঔৣ꧂
+    ⟞⟦────────────────🔰
+    ║╭──꧁✞*MAKER*✞꧂─
+    ║╠➣ *${prefix}sticker*
+    ║╠➣ *${prefix}toimg*
+    ║╠➣ *${prefix}phlogo*
+    ║╠➣ *{prefix}quotemaker*
+    ║╠➣ *${prefix}ninjalogo*
+    ║╠➣ *${prefix}glitch*
+    ║╠➣ *${prefix}text3d*
+    ║╰───────────────
+ 
+
+×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=              
+
+`
+}
+exports.help = help
+
+
+
+  
